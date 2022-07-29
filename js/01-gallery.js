24dc909b05df8d3f14d9cde63652e017920c0e05
Serhii Kaushnian <skaushnjan@gmail.com>
2022-07-29 19:39:43 +0200

    amend

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,13 +1,12 @@
-import { galleryItems } from "./gallery-items.js";
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const divEl = document.querySelector(".gallery");
+const divEl = document.querySelector('.gallery');
 
-const previewLinks = galleryItems.reduce(
-  (acc, { preview, original, description }) => {
+const previewLinks = galleryItems.reduce((acc, { preview, original, description }) => {
     return (
-      acc +
-      `<div class="gallery__item">
+        acc +
+        `<div class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
@@ -19,39 +18,33 @@ const previewLinks = galleryItems.reduce(
   </a>
 </div>`
     );
-  },
-  ""
-);
-divEl.insertAdjacentHTML("beforeend", previewLinks);
+}, '');
 
-divEl.addEventListener("click", function (event) {
-  event.preventDefault();
-  divEl.classList.add("listened");
+divEl.insertAdjacentHTML('beforeend', previewLinks);
 
-  if (event.target === event.currentTarget) return;
-  let instance = basicLightbox.create(
-    `
+divEl.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    if (event.target === event.currentTarget) return;
+    let instance = basicLightbox.create(
+        `
     <img src=${event.target.dataset.source} >
 `,
-    {
-      onClose: (instance) => {
-        window.removeEventListener("keydown", modalCloseOnEscPress);
-      },
+        {
+            onClose: instance => {
+                window.removeEventListener('keydown', modalCloseOnEscPress);
+            },
+        }
+    );
+    instance.src = event.target.dataset.source;
+
+    instance.show();
+
+    window.addEventListener('keydown', modalCloseOnEscPress, { once: true });
+
+    function modalCloseOnEscPress(event) {
+        if (event.code !== 'Escape') return;
+
+        instance.close();
     }
-  );
-  instance.src = event.target.dataset.source;
-
-  instance.show();
-
-  window.addEventListener("keydown", modalCloseOnEscPress, { once: true });
-
-  function modalCloseOnEscPress(event) {
-    if (event.code !== "Escape") return;
-
-    instance.close();
-
-    divEl.classList.remove("listened");
-
-    console.log(divEl.classList.contains("listened"));
-  }
 });
